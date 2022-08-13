83e86244752f5ec521563b950f719c7da021726c
dan <dan.debiazi@gmail.com>
2022-08-13 14:41:54 -0400

    Added CORS

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 import routes from './routes';
 
 const app = express();
+const cors = require('cors');
 
 mongoose.connect('mongodb://localhost:27017/WordsThatIKnowMongoDB')
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.listen(1987, () => {
