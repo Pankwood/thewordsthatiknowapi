d5848d44f47ee26695bbd7e7ef3a0d1298e6b297
dan <dan.debiazi@gmail.com>
2022-08-13 14:25:17 -0400

    Added find word method and improved create method

--- src/controller/WordController.ts
+++ src/controller/WordController.ts
@@ -2,11 +2,31 @@ import { Request, Response } from 'express';
 import Word from '../database/schemas/word';
 
 class WordController {
+    async find(request: Request, response: Response) {
+        try {
+            const words = await Word.find();
+            return response.json(words);
+        } catch (error) {
+            return response.status(500).send({
+                error: "Registration error",
+                message: error,
+            })
+        }
+    }
+
     async create(request: Request, response: Response) {
         try {
+            const wordExist = await Word.findOne(request.body);
+
+            if (wordExist)
+                return response.status(400).json({
+                    error: "Ooops",
+                    message: "Word already exists",
+                });
+
             const word = await Word.create(request.body);
             console.log(request.body);
-            response.json(word);
+            response.status(201).json(word);
         } catch (error) {
             return response.status(500).send({
                 error: "Registration error",
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import WordController from './controller/WordController';
 
 const routes = Router();
 
+routes.get("/word", WordController.find);
 routes.post("/word", WordController.create);
 
 export default routes;
