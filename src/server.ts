abf62b77c9f2068a9be21105339c35ec133132f0
dan <dan.debiazi@gmail.com>
2022-08-16 21:57:27 -0400

    Attempt to fix Vercel 2

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,14 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import routes from './routes';
-
-const expressVercel = require("express");
+const express = require("express");
 
 const app = express();
-const cors = require('cors');
-
-mongoose.connect('mongodb://localhost:27017/WordsThatIKnowMongoDB')
-
-app.use(express.json());
-app.use(cors());
-app.use(routes);
 
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
 });
 
 app.listen(5000, () => {
-    console.log('server is listening');
+    console.log("Running on port 5000.");
 });
 
+// Export the Express API
 module.exports = app;
