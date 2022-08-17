eeea4eda130c0cf86f2db11d66f89f2ffa58f1c7
dan <dan.debiazi@gmail.com>
2022-08-16 22:03:21 -0400

    Attempt to fix Vercel 4

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,16 @@
+import mongoose from 'mongoose';
+import routes from './routes';
+
 const express = require("express");
 
 const app = express();
+const cors = require('cors');
+
+mongoose.connect('mongodb://localhost:27017/WordsThatIKnowMongoDB')
+
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 app.get("/", (req, res) => {
     res.send("Express on Vercel");
