2a1d6e5ef91c8ba8896a13ec6c0ca8d4928f02ad
dan <dan.debiazi@gmail.com>
2022-08-25 19:33:04 -0400

    Set up Swagger on Vercel 13

--- src/server.ts
+++ src/server.ts
@@ -20,14 +20,10 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-const swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('../swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
-app.use(
-    '/api-docs',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerDocument)
-);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(5000, () => {
     console.debug("Running on port 5000.");
