a55fba90fd905164f3309282a20ed473ad478182
dan <dan.debiazi@gmail.com>
2022-08-25 18:28:31 -0400

    Set up Swagger on Vercel 11

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import mongoose from 'mongoose';
 import routes from './routes';
 
 const express = require("express");
+const cors = require('cors');
+const swaggerDocument = require('./swagger.json');
+const swaggerUi = require('swagger-ui-express');
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const app = express();
-const cors = require('cors');
 
 mongoose.connect(process.env.MONGODB_URI || "", {
     dbName: "WordsThatIKnowMongoDB"
@@ -16,15 +18,11 @@ mongoose.connect(process.env.MONGODB_URI || "", {
     .then(() => console.debug("Database connected!"))
     .catch(err => { console.debug(err) });
 
-const swaggerDocument = require('./swagger.json');
-const swaggerUi = require('swagger-ui-express');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //app.use(express.json());
 app.use(cors());
 app.use(routes);
-
-
-
+app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(5000, () => {
     console.debug("Running on port 5000.");
