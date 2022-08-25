ffe98698467f886defa98094abd552d20d05591d
dan <dan.debiazi@gmail.com>
2022-08-25 12:13:35 -0400

    Set up Swagger on Vercel

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ const swaggerUi = require('swagger-ui-express'),
     swaggerDocument = require('../swagger.json');
 
 app.use(
-    '/doc',
+    '/',
     swaggerUi.serve,
     swaggerUi.setup(swaggerDocument)
 );
