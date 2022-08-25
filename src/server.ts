0efe10d75c2f4e14eb49b79b7484a87729c160d9
dan <dan.debiazi@gmail.com>
2022-08-25 13:03:15 -0400

    Set up Swagger on Vercel 8

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI || "", {
     .then(() => console.debug("Database connected!"))
     .catch(err => { console.debug(err) });
 
-app.use(express.json());
+//app.use(express.json());
 app.use(cors());
 app.use(routes);
 
