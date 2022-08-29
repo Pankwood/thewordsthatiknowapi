5cda202d3d65d852ae20654a9ab2224d316d59b2
dan <dan.debiazi@gmail.com>
2022-08-29 00:53:55 -0400

    Set up Swagger on Vercel 51

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,23 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import routes from './routes';
-import bodyParser from 'body-parser';
-import path from 'path';
 
 
 
 const ROOT_FOLDER = path.join(__dirname, '..');
 const SRC_FOLDER = path.join(ROOT_FOLDER, 'src');
 
-const express = require("express");
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 const app = express();
-const cors = require('cors');
 
+app.set('trust proxy', 1); // trust first proxy
 
-mongoose.connect(process.env.MONGODB_URI || "", {
-    dbName: "WordsThatIKnowMongoDB"
-})
-    .then(() => console.debug("Database connected!"))
-    .catch(err => { console.debug(err) });
+// Security
+app.use(cors());
+
+app.use(routes);
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -30,14 +25,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(ROOT_FOLDER, 'build'), { index: false,  }));
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+// database
+mongoose.connect(process.env.MONGODB_URI || "", {
+    dbName: "WordsThatIKnowMongoDB"
+})
+    .then(() => console.debug("Database connected!"))
+    .catch(err => { console.debug(err) });
+
+
+app.use(express.static(path.join(ROOT_FOLDER, 'build'), { index: false }));
 app.use('/static', express.static(path.join(SRC_FOLDER, 'public')));
 app.use('/media', express.static(path.join(ROOT_FOLDER, 'public')));
-
-app.use(cors());
-app.use(routes);
 
 
 const swaggerUi = require('swagger-ui-express');
@@ -45,16 +48,16 @@ const swaggerDocument = require('./swagger.json');
 
 
 var options = {
+    explorer: true,
     customCssUrl: '/../swagger-ui.css'
 };
 
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
 
-
 app.listen(5000, () => {
     console.debug("Running on port 5000.");
 });
 
-// Export the Express API
-module.exports = app;
+
+export default app;
